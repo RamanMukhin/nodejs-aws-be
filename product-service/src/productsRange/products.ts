a14a79b3f0b4bd61cc4f49fdf3b77bf113684c0e
Raman Mukhin <Kextcf2018@tut.by>
2021-08-23 15:36:47 +0300

    feat: added point1

---
+++ product-service/src/productsRange/products.ts
@@ -0,0 +1,51 @@
+export const products = [
+  {
+    count: 1,
+    description: "Short Product Description1",
+    id: "1111ec4b-b10c-48c5-9345-fc73c48a80a2",
+    price: 1.1,
+    title: "FirstProduct"
+  },
+  {
+    count: 2,
+    description: "Short Product Description2",
+    id: "2222ec4b-b10c-48c5-9345-fc73c48a80a2",
+    price: 2.2,
+    title: "SecondProduct"
+  },
+  {
+    count: 3,
+    description: "Short Product Description3",
+    id: "3333ec4b-b10c-48c5-9345-fc73c48a80a2",
+    price: 3.3,
+    title: "ThirdProduct"
+  },
+  {
+    count: 4,
+    description: "Short Product Description4",
+    id: "4444ec4b-b10c-48c5-9345-fc73c48a80a2",
+    price: 4.4,
+    title: "FourthProduct"
+  },
+  {
+    count: 5,
+    description: "Short Product Description5",
+    id: "5555ec4b-b10c-48c5-9345-fc73c48a80a2",
+    price: 5.5,
+    title: "FifthProduct"
+  },
+  {
+    count: 6,
+    description: "Short Product Description6",
+    id: "6666ec4b-b10c-48c5-9345-fc73c48a80a2",
+    price: 6.6,
+    title: "SixthProduct"
+  },
+  {
+    count: 7,
+    description: "Short Product Description7",
+    id: "7777ec4b-b10c-48c5-9345-fc73c48a80a2",
+    price: 7.7,
+    title: "SeventhProduct"
+  },
+];
