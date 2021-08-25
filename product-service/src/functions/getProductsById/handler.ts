1daabbdc974f391fa538e387f37d1ad433d26e66
Raman Mukhin <Kextcf2018@tut.by>
2021-08-25 12:07:45 +0300

    feat: added error handling

--- product-service/src/functions/getProductsById/handler.ts
+++ product-service/src/functions/getProductsById/handler.ts
@@ -6,10 +6,20 @@ import { middyfy } from '@libs/lambda';
 
 import schema from './schema';
 import { products } from 'src/productsRange/products';
+import { Product } from '@functions/product';
+import { StatusCodes, getReasonPhrase } from 'http-status-codes';
 
 const getProductsById: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
-  const { productId } = event.pathParameters;
-  return formatJSONResponse(products.find((product) => product.id === productId));
+  try {
+    const { productId } = event.pathParameters;
+    const product: Product | undefined = products.find((product) => product.id === productId);
+    if (!product) {
+      return formatJSONResponse({}, StatusCodes.NOT_FOUND, { message: getReasonPhrase(StatusCodes.NOT_FOUND) });
+    }
+    return formatJSONResponse(product);
+  } catch (err) {
+    return formatJSONResponse({}, StatusCodes.INTERNAL_SERVER_ERROR, { message: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR) });
+  }
 }
 
 export const main = middyfy(getProductsById);
--- product-service/src/libs/apiGateway.ts
+++ product-service/src/libs/apiGateway.ts
@@ -1,16 +1,19 @@
+import { Product } from "@functions/product";
 import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
+import { StatusCodes } from "http-status-codes";
 import type { FromSchema } from "json-schema-to-ts";
 
 type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
 export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
 
-export const formatJSONResponse = (response) => {
+export const formatJSONResponse = (product: Product | Product[] | {}, statusCode: number = StatusCodes.OK, options: { message?: string } = {}) => {
+  const response = options.message ? options : product;
   return {
-    statusCode: 200,
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
+    statusCode,
     body: JSON.stringify(response)
   }
 }
