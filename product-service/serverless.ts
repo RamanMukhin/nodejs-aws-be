7e4154a2e1414c57d26dfc357f175a4dd372f45a
Raman Mukhin <Kextcf2018@tut.by>
2021-09-07 21:58:49 +0300

    fix: fixed env in serverless

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -3,7 +3,7 @@ import type { AWS } from '@serverless/typescript';
 import getProductsList from '@functions/getProductsList';
 import getProductsById from '@functions/getProductsById';
 import postProduct from '@functions/postProduct';
-import { PG_HOST, PG_PORT, PG_DB, PG_USER, PG_PASSWORD } from './src/common/config';
+// import { PG_HOST, PG_PORT, PG_DB, PG_USER, PG_PASSWORD } from './src/common/config';
 // import { DEFAULT_MAX_VERSION } from 'tls';
 
 const serverlessConfiguration: AWS = {
@@ -27,11 +27,11 @@ const serverlessConfiguration: AWS = {
     },
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      PG_HOST,
-      PG_PORT,
-      PG_DB,
-      PG_USER,
-      PG_PASSWORD,
+      PG_HOST: '${env:PG_HOST}',
+      PG_PORT: '${env:PG_PORT}',
+      PG_DB: '${env:PG_DB}',
+      PG_USER: '${env:PG_USER}',
+      PG_PASSWORD: '${env:PG_PASSWORD}',
     },
     lambdaHashingVersion: '20201221',
   },
