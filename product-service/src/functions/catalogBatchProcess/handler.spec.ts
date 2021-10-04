93e359086f33d08b13ae0f55fc68f730d48fbaf1
Raman Mukhin <Kextcf2018@tut.by>
2021-10-05 00:20:10 +0300

    feat: added unit tests

--- product-service/src/functions/catalogBatchProcess/handler.spec.ts
+++ product-service/src/functions/catalogBatchProcess/handler.spec.ts
@@ -1,46 +1,86 @@
-// import { mocked } from 'ts-jest/utils';
-// import { Handler } from 'aws-lambda';
-// import dotenv from 'dotenv'
+import { mocked } from 'ts-jest/utils';
+import { Handler } from 'aws-lambda';
 
-// import { middyfy } from '@libs/lambda';
-// import { StatusCodes } from 'http-status-codes';
+import { middyfy } from '@libs/lambda';
+import { mockClient } from "aws-sdk-client-mock";
+import { SNSClient, PublishCommand } from "@aws-sdk/client-sns";
+import { Client } from 'pg';
 
-// jest.mock('@libs/lambda');
-// dotenv.config();
 
-// describe('importProductsFile', () => {
-//   let main;
-//   let mockedMiddyfy: jest.MockedFunction<typeof middyfy>;
+jest.mock('pg', () => {
+  const mClient = {
+    connect: jest.fn(),
+    query: jest.fn(),
+    end: jest.fn(),
+  };
+  return { Client: jest.fn(() => mClient) };
+});
 
-//   beforeEach(async () => {
-//     mockedMiddyfy = mocked(middyfy);
-//     mockedMiddyfy.mockImplementation((handler: Handler) => {
-//       return handler as never;
-//     });
+jest.mock('@libs/lambda');
 
-//     main = (await import('./handler')).main;
-//   });
+const SNSClientMock = mockClient(SNSClient);
 
-//   afterEach(() => {
-//     jest.resetModules();
-//   });
+const flowerInDb = {
+  id: "8e9ad1cb-9f93-46d5-beba-c2c930786f5f",
+  description: "Short Product Description1",
+  title: "Rose",
+  sort: "avalansh",
+  height: 30,
+  price: 1.1,
+  count: 300
+};
 
-//   it('should return SignedURL', async () => {
-//     const BUCKET = 'shop-import-service';
-//     const REGION = 'eu-west-1';
-//     const FOLDER = 'uploaded';
-//     const TESTFILE = "test.csv";
+describe('catalogBatchProcess', () => {
+  let client;
+  let main;
+  let mockedMiddyfy: jest.MockedFunction<typeof middyfy>;
 
-//     const event = {
-//       queryStringParameters: {
-//         name: TESTFILE,
-//       },
-//     };
+  beforeEach(async () => {
+    client = new Client();
+    SNSClientMock.reset();
+    mockedMiddyfy = mocked(middyfy);
+    mockedMiddyfy.mockImplementation((handler: Handler) => {
+      return handler as never;
+    });
 
-//     const actual = await main(event);
+    main = (await import('./handler')).main;
+  });
 
-//     expect(JSON.parse(actual.body).startsWith(`https://${BUCKET}.s3.${REGION}.amazonaws.com/${FOLDER}/${TESTFILE}`)).toEqual(true);
-//     expect(actual.statusCode).toEqual(StatusCodes.OK);
-//   });
+  afterEach(() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+  });
 
-// });
+  it('should saved in db recived flowers', async () => {
+    client.connect.mockResolvedValueOnce(1);
+    client.query.mockResolvedValueOnce(1).mockResolvedValueOnce({ rows: [flowerInDb], rowCount: 1 });
+    client.end.mockResolvedValueOnce(1);
+
+    const event = {
+      Records: [
+        {
+          body: '{"id":"5555ec4b-b10c-48c5-9345-fc73c48a80a2","description":"Short Product Description5","title":"Rose","sort":"missPiggi","height":"70","count":"5","price":"5.5"}',
+        },
+        {
+          body: '{"id":"7777ec4b-b10c-48c5-9345-fc73c48a80a2","description":"Short Product Description7","title":"Rose","sort":"vau","height":"70","count":"7","price":"7.7"}',
+        }
+      ]
+    };
+
+    SNSClientMock
+      .on(PublishCommand)
+      .resolves({
+        MessageId: 'MessageId',
+        SequenceNumber: 'SequenceNumber',
+      })
+
+    const actual = await main(event);
+
+    expect(client.connect).toHaveBeenCalledTimes(2);
+    expect(client.query).toHaveBeenCalledTimes(1);
+    expect(client.end).toHaveBeenCalledTimes(0);
+    expect(actual).toBeUndefined;
+
+  });
+
+});
--- product-service/src/functions/getProductsList/handler.spec.ts
+++ product-service/src/functions/getProductsList/handler.spec.ts
@@ -2,16 +2,36 @@ import { mocked } from 'ts-jest/utils';
 import { Handler } from 'aws-lambda';
 
 import { middyfy } from '@libs/lambda';
-import { products } from '../../productsRange/products';
-import { StatusCodes } from 'http-status-codes';
+import { Client } from 'pg';
+
+jest.mock('pg', () => {
+  const mClient = {
+    connect: jest.fn(),
+    query: jest.fn(),
+    end: jest.fn(),
+  };
+  return { Client: jest.fn(() => mClient) };
+});
 
 jest.mock('@libs/lambda');
 
+const flowerInDb = {
+  id: "8e9ad1cb-9f93-46d5-beba-c2c930786f5f",
+  description: "Short Product Description1",
+  title: "Rose",
+  sort: "avalansh",
+  height: 30,
+  price: 1.1,
+  count: 300
+};
+
 describe('getProductList', () => {
+  let client;
   let main;
   let mockedMiddyfy: jest.MockedFunction<typeof middyfy>;
 
   beforeEach(async () => {
+    client = new Client();
     mockedMiddyfy = mocked(middyfy);
     mockedMiddyfy.mockImplementation((handler: Handler) => {
       return handler as never;
@@ -22,11 +42,14 @@ describe('getProductList', () => {
 
   afterEach(() => {
     jest.resetModules();
+    jest.clearAllMocks();
   });
 
   it('should return list of products', async () => {
-    const equalValue = JSON.stringify(products);
-    const statusCode = StatusCodes.OK;
+    client.connect.mockResolvedValueOnce(1);
+    client.query.mockResolvedValueOnce({ rows: [flowerInDb], rowCount: 1 });
+    client.end.mockResolvedValueOnce(1);
+    const equalValue = [flowerInDb];
 
     const event = {
       body: {}
@@ -34,8 +57,11 @@ describe('getProductList', () => {
 
     const actual = await main(event);
 
-    expect(actual.body).toEqual(equalValue);
-    expect(actual.statusCode).toEqual(statusCode);
+    expect(actual).toEqual(equalValue);
+    expect(Array.isArray(actual)).toBe(true);
+    expect(client.connect).toHaveBeenCalledTimes(1);
+    expect(client.query).toHaveBeenCalledTimes(1);
+    expect(client.end).toHaveBeenCalledTimes(1);
   });
 
 });
